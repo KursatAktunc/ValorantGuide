3b4200e7b35c821d07c59941eedd2e659b94707c
Kürşat Aktunç <aktunc4498@gmail.com>
2022-06-03 13:18:23 +0300

    Navigation Drawer added

--- app/src/main/java/com/kursat/valorantguide/ui/agents/AgentsListViewModel.kt
+++ app/src/main/java/com/kursat/valorantguide/ui/agents/AgentsListViewModel.kt
@@ -1,7 +1,9 @@
 package com.kursat.valorantguide.ui.agents
 
 import com.kursat.valorantguide.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class AgentsListViewModel @Inject constructor() : BaseViewModel() {
 }
